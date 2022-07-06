6d1264103a3f27fd04bc57ea5b176779521ce0cc
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-07-06 18:06:35 +0200

    if...else statements

---
+++ stuff/federico-lopez/js/data-types/if...else/main.js
@@ -0,0 +1,21 @@
+function whatToDo(clima) {
+    if (clima === 'llueve') {
+        console.log('llevo paraguas')
+    
+    } else if (clima === 'soleado') {
+         console.log('llevo traje de baño')   
+    
+    } else if (clima === 'tormenta eléctrica') {
+        console.log('me quedo en casa')
+    
+    } else if (clima === 'soleado') {
+        console.log('me quedo en casa')
+    
+    } else {
+        console.log('salgo normalmente')
+    }    
+}
+
+whatToDo('soleado')
+whatToDo('nublado')
+whatToDo('llueve')
