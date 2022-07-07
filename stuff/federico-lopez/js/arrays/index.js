9c6f61d1a38094a0f45610d6623eea9eb6372054
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-07-07 14:45:48 +0200

    arrays demo

---
+++ stuff/federico-lopez/js/arrays/index.js
@@ -0,0 +1,20 @@
+const emptyArray = []
+const numbers = [0, 1, 2, 3, 4, 5]
+const letters = ['a', 'b', 'c', 'd', 'e']
+const booleans = [true, false, false, false, true]
+
+const array = [2, 'hello', false, 3, null, undefined, null]
+
+/*const obj = {
+    0: 2,
+    1: 'hello',
+    2: false,
+    3: 3,
+    4: null,
+    5: undefined,
+    6: null
+}*/
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
