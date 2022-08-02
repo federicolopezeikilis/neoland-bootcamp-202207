b76f7fc38bb6f3338820d581a9d012643c83c3a8
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-08-02 13:32:40 +0200

    update

--- staff/federico-lopez/cachay/Cachay.prototype.reduce.js
+++ staff/federico-lopez/cachay/Cachay.prototype.reduce.js
@@ -1,7 +1,9 @@
 Cachay.prototype.reduce = function (callback, initialValue) {
-    let accumulator = initialValue
+    let accumulator = initialValue !== undefined ? initialValue : this[0]
 
-    for (let i = 0; i < this.length; i++) {
+    const firstIndex = initialValue !== undefined ? 0 : 1
+
+    for (let i = firstIndex; i < this.length; i++) {
         const element = this[i]
 
         accumulator = callback(accumulator, element)
--- staff/federico-lopez/cachay/Cachay.prototype.reduce.spec.js
+++ staff/federico-lopez/cachay/Cachay.prototype.reduce.spec.js
@@ -2,13 +2,13 @@ describe('Cachay.prototype.reduce', () => {
     it('should sum all values in a cachay of numbers', () => {
         const cachay1 = new Cachay(1, 2, 3, 4)
 
-        const initialValue = 0;
+        const initialValue = 10
 
         const sumValues = (previousValue, currentValue) => previousValue + currentValue
 
         const result = cachay1.reduce(sumValues, initialValue)
 
-        expect(result).to.equal(10)
+        expect(result).to.equal(20)
         expect(result).to.be.a('number')
     })
 
@@ -22,4 +22,30 @@ describe('Cachay.prototype.reduce', () => {
         expect(result).to.equal(9000)
         expect(result).to.be.a('number')
     })
+
+    it('should sum all values in a cachay of numbers', () => {
+        const cachay1 = new Cachay(1, 2, 3, 4)
+
+        const sumValues = (previousValue, currentValue) => previousValue + currentValue
+
+        debugger
+        const result = cachay1.reduce(sumValues)
+
+        expect(result).to.equal(10)
+        expect(result).to.be.a('number')
+    })
+
+    // it('', () => {
+    //     const cachay1 = new Cachay(100, 500, 8000, 2, 9000, 3)
+
+    //     const getMaxValueUntilIndex3 = (previousValue, currentValue, index) => {
+    //         if(index < 4)
+    //             return Math.max(previousValue, currentValue)
+    //     }
+
+    //     const result = cachay1.reduce(getMaxValueUntilindex3)
+
+    //     expect(result).to.equal(8000)
+    //     expect(result).to.be.a('number')
+    // })
 })
