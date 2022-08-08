4a42d18f8f4851ada52c3f5475fe4d0cca495df7
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-08-08 18:30:04 +0200

    update hangman

--- staff/federico-lopez/hangman/components/App.jsx
+++ staff/federico-lopez/hangman/components/App.jsx
@@ -2,16 +2,50 @@ class App extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = { view: 'select word', result: null }
+        this.state = { view: 'select word', result: null, wordShown: null, counter: 10}
     }
 
     handleOnChooseWordFormSubmit = event => {
-        event.preventDefault()
-
         const wordSelected = event.target.input.value
 
-        this.setState({ result: wordSelected, view: 'playing' })
+        event.target.reset()
+
+        this.setState({ result: wordSelected, view: 'playing', wordShown: "_".repeat(wordSelected.length) })
     }
+
+    handleOnCharacterForm = event => {
+        const charTried = event.target.input.value
+
+        event.target.reset()
+
+        // VERIFICO SI EL CARACTER ESTÁ EN EL RESULTADO
+        if (this.state.result.includes(charTried)) {
+            const indexes = []
+            
+            // GUARDO LOS ÍNDICES EN LOS QUE EL CARACTER SE ENCUENTRA EN EL RESULTADO
+            for (let i = 0; i < this.state.result.length; i++)
+                if (this.state.result[i] === charTried)
+                    indexes.push(i)
+
+            // TRANSFORMO LA PALABRA MOSTRADA EN ARRAY
+            let newWordShown = this.state.wordShown.split('')
+
+            // CAMBIO LOS "_" POR LA LETRA ELEGIDA
+            indexes.forEach(index => {
+                newWordShown[index] = charTried
+            })
+
+            // MODIFICO FINALMENTE EL ESTADO
+            this.setState({ wordShown: newWordShown.join('')})
+
+        } else {
+            const newCounter = this.state.counter - 1
+
+            this.setState({ counter: newCounter})
+        }
+    }
+
+
 
     render() {
         return (
@@ -23,8 +57,8 @@ class App extends React.Component {
 
                 {this.state.view === "playing" &&
                     <>
-                        <h1>{"_ ".repeat(this.state.result.length)}</h1>
-                        <Form placeholder="enter a character" buttonText="TRY" />
+                        <h1 className="wordShown">{this.state.wordShown}</h1>
+                        <Form placeholder="enter a character" buttonText="TRY" onSubmit={this.handleOnCharacterForm} />
                     </>
                 }
 
--- staff/federico-lopez/hangman/components/Form.jsx
+++ staff/federico-lopez/hangman/components/Form.jsx
@@ -1,6 +1,13 @@
 function Form(props) {
     return (
-        <form className="form" onSubmit={props.onSubmit}>
+        <form
+            className="form"
+            onSubmit={function (event) {
+                event.preventDefault()
+                
+                props.onSubmit(event)
+            }}>
+
             <input placeholder={props.placeholder} name="input" />
             <button type="submit">{props.buttonText}</button>
         </form>
